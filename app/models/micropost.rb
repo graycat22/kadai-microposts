8d9058a8c6f6cc992b04bac13328e26ce7d14109
kame.andy <asaneko22@gmail.com>
2022-03-07 18:30:58 +0000

    for assignment

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_many :favorites
+  has_many :like_users, through: :favorites, source: :user, dependent: :destroy
+  
   validates :content, presence: true, length: { maximum: 255 }
 end
