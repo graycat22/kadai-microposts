ef3b4f7e59cf5c7397cca997137a6d165a834a2f
kame.andy <asaneko22@gmail.com>
2022-03-07 04:05:18 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,6 +2,6 @@ class ToppagesController < ApplicationController
   before_action :require_user_logged_in
   def index
     @micropost = current_user.microposts.build  # form_with 用
-    @pagy, @microposts = pagy(current_user.microposts.order(id: :desc))
+    @pagy, @microposts = pagy(current_user.feed_microposts.order(id: :desc))
   end
 end
